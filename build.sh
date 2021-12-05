19c61a1cab3ae969f7258a2453634d85ebb5cf53
Lera Rosalene <lerarosalene@outlook.com>
2021-12-05 12:12:21 +0300

    use coffeescript

--- build.sh
+++ build.sh
@@ -9,6 +9,11 @@ which minify
 mkdir -p dist
 
 minify src/index.html > dist/index.html
-minify src/index.js > dist/index.js
+coffee -cmo dist/index.js src/index.coffee
+terser -cm -o dist/index.min.js \
+  --source-map "content=dist/index.js.map" \
+  --source-map "url=index.min.js.map" \
+  -- dist/index.js
+rm dist/index.js dist/index.js.map
 minify src/index.css > dist/index.css
 cp src/favicon.ico dist/
